83b9f1f9bf72f36f3c99dea150c156877df490a4
Magnus Kronnäs <mail@example.co>
2014-11-01 00:38:38 +0100

    Added some tests

---
+++ html/headline_test.go
@@ -0,0 +1,22 @@
+package html
+
+import "testing"
+
+func Test(t *testing.T) {
+    var tests = []struct {
+        s, want string
+    }{
+        {"Hello", "<h1>Hello</h1>"},
+        {"Good Bye!", "<h1>Good Bye!</h1>"},
+        {"ハイ", "<h1>ハイ</h1>"},
+        {"<b>Hello</b>", "<h1><b>Hello</b></h1>"},
+        {"", "<h1></h1>"},
+    }
+
+    for _, c := range tests {
+        got := Headline(c.s)
+        if got != c.want {
+            t.Errorf("Headline(%s) == %s, want %s", c.s, got, c.want)
+        }
+    }
+}
