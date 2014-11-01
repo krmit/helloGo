bb053bc4aff95d2fcb2d1991e5240f183e79015a
Magnus Kronnäs <mail@example.co>
2014-11-01 01:32:05 +0100

    Added command-line flag for fun

--- helloGo.go
+++ helloGo.go
@@ -3,9 +3,26 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"github.com/krmit/helloGo/html"
 	)
 
 func main() {
-	fmt.Printf(html.Headline("Hello GO!"))
+	
+	//Parse commadline flags.	
+	repeat := 0
+	text := ""
+	headline := true 
+	flag.IntVar(&repeat, "r", 1, "Number of time to writte message.")
+	flag.StringVar(&text, "t", "Hello Go!", "Test that message should containe.")
+	flag.BoolVar(&headline, "hl", true, "If it should be a headline")
+	flag.Parse()
+	
+	for i := 0; i < repeat; i++ {
+		if headline {
+				fmt.Printf(html.Headline(text))
+			} else {
+			fmt.Printf(text)
+		}		
+	}
 }
