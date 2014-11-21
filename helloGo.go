778fca68176855e92c9aa03e57b60739c77c9f23
Magnus Kronnäs <mail@example.co>
2014-11-21 15:22:41 +0100

    Change from flag to kingpin lib.

--- helloGo.go
+++ helloGo.go
@@ -2,27 +2,25 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"gopkg.in/alecthomas/kingpin.v1"
 	"github.com/krmit/helloGo/html"
 )
 
 func main() {
 
 	//Parse commadline flags.
-	repeat := 0
-	text := ""
-	headline := true
-	flag.IntVar(&repeat, "r", 1, "Number of time to writte message.")
-	flag.StringVar(&text, "t", "Hello Go!", "Test that message should containe.")
-	flag.BoolVar(&headline, "hl", true, "If it should be a headline")
-	flag.Parse()
-
-	for i := 0; i < repeat; i++ {
-		if headline {
-			fmt.Printf(html.Headline(text))
+	repeat := kingpin.Flag("r", "Number of time to writte message.").Default("1").Int()
+	text := kingpin.Flag("t", "Text that the message should containe.").Default("Hello Go!").String()
+	headline := kingpin.Flag("hl", "If it should be a headline").Default("false").Bool()
+	
+	kingpin.Parse()
+	
+	for i := 0; i < *repeat; i++ {
+		if *headline {
+			fmt.Printf(html.Headline(*text))
 		} else {
-			fmt.Printf(text)
+			fmt.Printf(*text)
 		}
 	}
 }
